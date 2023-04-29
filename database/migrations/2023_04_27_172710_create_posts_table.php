81545decfb50bab4a72d0556c75835c2d5cdd7ba
adamalireza <adamalirezasumarna@gmail.com>
2023-04-29 15:08:09 +0800

    first commit

---
+++ database/migrations/2023_04_27_172710_create_posts_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+     public function up()
+{
+    Schema::create('posts', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->string('slug')->unique();
+        $table->unsignedInteger('category_id');
+        $table->unsignedInteger('user_id');
+        $table->text('content');
+        $table->string('image');
+        $table->text('description');
+        $table->timestamps();
+    });
+}
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
